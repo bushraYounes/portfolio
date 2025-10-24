80e348c67e741ef300b7ec219de098b8a8ef4b7f
bushraYounes <boushra.ali.younes@gmail.com>
2025-10-24 03:44:03 +0300

    lightMode

--- src/components/ThemeToggle.jsx
+++ src/components/ThemeToggle.jsx
@@ -3,44 +3,44 @@ import React, { useEffect, useState } from 'react'
 import { cn } from "../../src/lib/utils"
 
 const ThemeToggle = () => {
-  const [isDarkMode, setIsDarkMode] = useState(false);
+  const [isDarkMode, setIsDarkMode] = useState(true);
 
   useEffect(() => {
     const storedTheme = localStorage.getItem("theme")
-    if (storedTheme=="dark"){
+    if (storedTheme == "light") {
       setIsDarkMode(true)
-      document.documentElement.classList.add("dark");
+      document.documentElement.classList.add("light");
     }
-    else{
+    else {
       setIsDarkMode(false)
-      document.documentElement.classList.remove("dark");
+      document.documentElement.classList.remove("light");
     }
   }, []);
 
   const toggleTheme = () => {
     if (isDarkMode) {
-      document.documentElement.classList.remove("dark");
-      localStorage.setItem("theme", "light");
+      document.documentElement.classList.remove("light");
+      localStorage.setItem("theme", "dark");
       setIsDarkMode(false);
     }
     else {
-      document.documentElement.classList.add("dark");
-      localStorage.setItem("theme", "dark");
+      document.documentElement.classList.add("light");
+      localStorage.setItem("theme", "light");
       setIsDarkMode(true);
     }
   }
   return (
-    <button onClick={toggleTheme} 
-    className={cn("fixed max-sm:top-4.7 top-5 right-5 z-50 p-2 rounded-full transition-colors duration-300",
-      "focus:outline-hidden",
-      "hover:cursor-pointer"
-    )}
-      >
+    <button onClick={toggleTheme}
+      className={cn("fixed max-sm:top-4.7 top-5 right-5 z-50 p-2 rounded-full transition-colors duration-300",
+        "focus:outline-hidden",
+        "hover:cursor-pointer"
+      )}
+    >
       {""}
       {isDarkMode ? (
-        <Sun className='h-6 w-6 text-yellow-300' />
-      ) : (
         <Moon className='h-6 w-6 text-blue-900' />
+      ) : (
+        <Sun className='h-6 w-6 text-yellow-300' />
       )
       }
 
